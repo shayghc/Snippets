7e6b8c821f5c545fb28677c1a65482779592f3c9
Seamus Connolly <sghconnolly@gmail.com>
2018-02-05 08:36:55 +0000

    Add file with practical example of promise use
    /* This is a practical example of the use of a promises
     *
     * Attribution: From YouTube video - https://www.youtube.com/watch?v=IGYxfTTpoFg
     * 'Promises - Beau Teaches JavaScript'

---
+++ js/practical-promise-example.js
@@ -0,0 +1,30 @@
+/* This is a practical example of the use of a promises
+ *
+ * Attribution: From YouTube video - https://www.youtube.com/watch?v=IGYxfTTpoFg
+ * 'Promises - Beau Teaches JavaScript'
+ *
+ */
+
+let promiseCount = 0;
+
+function testPromise() {
+    let thisPromiseCount = ++promiseCount;
+    console.log(thisPromiseCount + ': Started - Sync code started');
+
+    let p1 = new Promise(function(resolve,reject) {
+        console.log(thisPromiseCount + 'Promise started - Async code started');
+        // Example use of setTimeout - in this case to provide asynchronous responses
+        window.setTimeout(
+            function() {
+                resolve(thisPromiseCount);
+            }, Math.random() * 2000 + 1000);
+    });
+
+    p1.then(function(val) {
+        console.log(val + ': Promise fulfilled - Async code terminated');
+    }).catch(function(reason) {
+        console.log('Handle rejected promise ('+reason+') here.');
+    });
+
+    console.log(thisPromiseCount + ': Promise made - Sync code termination')
+}
